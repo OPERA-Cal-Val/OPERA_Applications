aa0b3c6466846f4be1101f2d849713cc9d4c181a
gracebato <m.grace.bato@gmail.com>
2023-06-02 18:31:34 -0700

    Added coherence calculation and visualization.

--- src/cslc_utils.py
+++ src/cslc_utils.py
@@ -74,8 +74,8 @@ def custom_merge(old_data, new_data, old_nodata, new_nodata, **kwargs):
     old_data[mask] = new_data[mask]
 
 # Convert each pixel to RGB, adjusting colorscale relative to data range
-def colorize(array, cmap='RdBu'):
-    normed_data = (array - array.min()) / (array.max() - array.min())    
+def colorize(array=[], cmap='RdBu', cmin=[], cmax=[]):
+    normed_data = (array - cmin) / (cmax - cmin)    
     cm = plt.cm.get_cmap(cmap)
     return cm(normed_data) 
 
@@ -128,3 +128,59 @@ def getbasemaps():
     }
 
     return basemaps
+
+def scale_amp(img):
+    out = 20*np.log10(np.abs(img))
+    
+    return np.clip(out, np.nanmean(out)-np.nanstd(out),np.nanmean(out)+np.nanstd(out))
+
+# Modified after https://github.com/opera-adt/dolphin/blob/4289be10e019da9c8da6c659370601ea96e63fe9/src/dolphin/interferogram.py#L509-L552
+def moving_window_mean(image, size) -> np.ndarray:
+    """Calculate the mean of a moving window of size `size`.
+
+    Parameters
+    ----------
+    image : ndarray
+        input image
+    size : int or tuple of int
+        Window size. If a single int, the window is square.
+        If a tuple of (row_size, col_size), the window can be rectangular.
+
+    Returns
+    -------
+    ndarray
+        image the same size as `image`, where each pixel is the mean
+        of the corresponding window.
+    """
+    if isinstance(size, int):
+        size = (size, size)
+    if len(size) != 2:
+        raise ValueError("size must be a single int or a tuple of 2 ints")
+    if size[0] % 2 == 0 or size[1] % 2 == 0:
+        size = tuple(map(sum, zip(size, (1,1))))
+
+    row_size, col_size = size
+    row_pad = row_size // 2
+    col_pad = col_size // 2
+
+    # Pad the image with zeros
+    image_padded = np.pad(
+        image, ((row_pad + 1, row_pad), (col_pad + 1, col_pad)), mode="constant"
+    )
+
+    # Calculate the cumulative sum of the image
+    integral_img = np.cumsum(np.cumsum(image_padded, axis=0), axis=1)
+    if not np.iscomplexobj(integral_img):
+        integral_img = integral_img.astype(float)
+
+    # Calculate the mean of the moving window
+    # Uses the algorithm from https://en.wikipedia.org/wiki/Summed-area_table
+    window_mean = (
+        integral_img[row_size:, col_size:]
+        - integral_img[:-row_size, col_size:]
+        - integral_img[row_size:, :-col_size]
+        + integral_img[:-row_size, :-col_size]
+    )
+    window_mean /= row_size * col_size
+    
+    return window_mean
